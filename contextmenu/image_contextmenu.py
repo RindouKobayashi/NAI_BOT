633da2f0b7f32e50df190f6d8881c47acc67bbee
RindouKobayashi <tingwei_96@hotmail.com>
2024-10-05 13:56:57 +0800

    feat: Implement image tagging using wd-tagger
    Adds a new context menu option to get tags for images using the wd-tagger model. Users can now right-click on an image and select "Get tags using wd-tagger" to retrieve a list of predicted tags. The confidence levels for object detection are now displayed as percentages for better readability.

--- contextmenu/image_contextmenu.py
+++ contextmenu/image_contextmenu.py
@@ -36,7 +36,35 @@ def contextmenu(bot: commands.Bot):
                     embed.set_image(url=attachment)
                     embed.add_field(
                         name="Confidence Levels",
-                        value="\n".join(f"{key}: `{value}`" for key, value in sub_dict['confidence_levels'].items()),
+                        value="\n".join(f"{key}: `{value*100:.2f}%`" for key, value in sub_dict['confidence_levels'].items()),
+                        inline=False
+                    )
+                    message = await interaction.followup.send(embed=embed)
+        else:
+            await interaction.response.send_message("No image found in message", ephemeral=True)
+
+    @bot.tree.context_menu(name="Get tags using wd-tagger")
+    async def show_tags(interaction: discord.Interaction, message: discord.Message):
+        logger.info(f"CONTEXTMENU 'GET_TAGS' USED BY: {interaction.user} ({interaction.user.id})")
+        if message.attachments:
+            await interaction.response.send_message("Getting tags...", delete_after=10)
+            tags_dict = {}
+            for attachment in message.attachments:
+                if attachment.content_type.startswith("image"):
+                    tags = wd_tagger.predict(attachment.url, type="get_tags")
+                    tags_dict[attachment.url] = tags
+
+            if tags_dict:
+
+                for attachment, sub_dict in tags_dict.items():
+                    embed = discord.Embed(
+                        title="Wd-tagger Tags",
+                        description=f"Tags for {attachment}",
+                    )
+                    embed.set_image(url=attachment)
+                    embed.add_field(
+                        name="Tags",
+                        value=", ".join(f"{tags}" for tags in sub_dict),
                         inline=False
                     )
                     message = await interaction.followup.send(embed=embed)
--- core/wd_tagger.py
+++ core/wd_tagger.py
@@ -10,7 +10,7 @@ try:
 except Exception as e:
     logger.error(f"WD-TAGGER: {e}")
 
-def predict(image_url):
+def predict(image_url, type:str = "check_nsfw"):
     #TODO: Add logic to predict image, still in testing
     #LINK: https://huggingface.co/spaces/SmolRabbit/wd-tagger
     result = client.predict(
@@ -25,20 +25,23 @@ def predict(image_url):
     # Extract the confidence data from the result
     confidence_data = result[1]['confidences']
     #character_data = result[2]['confidences']
+    tags_data = result[3]['confidences']
 
     # Create a dictionary with the confidence data
     confidence_levels = {item['label']: item['confidence'] for item in confidence_data}
+    # Create a dictionary with the tags data
+    tags = {item['label']: item['confidence'] for item in tags_data}
     # Get the highest confidence level item
     highest_confidence_level = max(confidence_levels, key=confidence_levels.get)
     #logger.info(f"WD-TAGGER: {confidence_levels}")
     #logger.info(f"WD-TAGGER: {character_data}")
 
     # Check confidence_levels and see if it's above a certain threshold
-    if confidence_levels['explicit'] > 0.2:
-        return confidence_levels, highest_confidence_level, True
-    elif confidence_levels['questionable'] > 0.2:
-        return confidence_levels, highest_confidence_level, True
-    elif (confidence_levels['general'] + confidence_levels['sensitive']) < 0.5:
-        return confidence_levels, highest_confidence_level, True
-    else:
-        return confidence_levels, highest_confidence_level, False
+    if type == "check_nsfw":
+        if confidence_levels['explicit'] + confidence_levels['questionable'] > 0.15:
+            return confidence_levels, highest_confidence_level, True
+        else:
+            return confidence_levels, highest_confidence_level, False
+    
+    if type == "get_tags":
+        return tags
